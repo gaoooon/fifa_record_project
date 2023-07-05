210ad9b09bdbf3153d013c9f804c11510c6b4754
gaoooon <gaon9749@gmail.com>
2023-07-05 21:10:24 +0900

    update

--- script.js
+++ script.js
@@ -85,7 +85,81 @@ const inputSubmit = document.getElementById("inputSubmit");
 //const playerName = document.getElementById("playerName");
 const playerHistory = document.createElement("div");
 const playerImage = document.createElement("img");
+const level = document.createElement("div");
+const matchRecord0 = document.createElement("div");
+const matchRecord1 = document.createElement("div");
+const matchRecord2 = document.createElement("div");
+const matchRecord3 = document.createElement("div");
+const matchRecord4 = document.createElement("div");
 
+// function onSubmitNickname(event) {
+//   event.preventDefault();
+//   const Nickname = inputNickname.value;
+//   const information = document.getElementById("NicknameHistory");
+
+//   const URL_NICKNAME = `https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=${Nickname}`;
+//   fetch(URL_NICKNAME, {
+//     headers: {
+//       Authorization: API_KEY,
+//     },
+//   })
+//     .then((Response) => Response.json())
+//     .then((data) => {
+//       console.log(data);
+//       level.innerText = `level. ${data.level}`;
+//       information.appendChild(level);
+//       const playerID = data.accessId;
+//       const URL_HISTORY = `https://api.nexon.co.kr/fifaonline4/v1.0/users/${playerID}/maxdivision`;
+//       const URL_MATCH = `https://api.nexon.co.kr/fifaonline4/v1.0/users/${playerID}/matches?matchtype=50&offset=1&limit=5`;
+//       fetch(URL_HISTORY, {
+//         headers: {
+//           Authorization: API_KEY,
+//         },
+//       })
+//         .then((Response) => Response.json())
+//         .then((HistoryData) => {
+//           Division.forEach((d) => {
+//             if (d.divisionId === HistoryData[0].division) {
+//               playerHistory.innerText = d.divisionName;
+//               playerImage.src = `image/ico_rank${d.divisionId}.png`;
+//               information.appendChild(playerHistory);
+//               information.appendChild(playerImage);
+//             }
+//           });
+//         });
+//       const matchResult = [];
+//       //
+//       fetch(URL_MATCH, {
+//         headers: {
+//           Authorization: API_KEY,
+//         },
+//       })
+//         .then((Response) => Response.json())
+//         .then((matchData) => {
+//           console.log(matchData);
+//           matchData.forEach((d) => {
+//             fetch(`https://api.nexon.co.kr/fifaonline4/v1.0/matches/${d}`, {
+//               headers: {
+//                 Authorization: API_KEY,
+//               },
+//             })
+//               .then((Response) => Response.json())
+//               .then((detailData) => {
+//                 console.log(2);
+//                 let time = detailData.matchDate;
+//                 time = time.replace(/-/g, "");
+//                 time = time.replace(/T/g, "");
+//                 time = time.replace(/:/g, "");
+//                 matchResult.push({
+//                   date: time,
+//                   result: detailData.matchInfo[0].matchDetail.matchResult,
+//                 });
+//               });
+//           });
+//           console.log(1);
+//         });
+//     });
+// }
 function onSubmitNickname(event) {
   event.preventDefault();
   const Nickname = inputNickname.value;
@@ -100,10 +174,12 @@ function onSubmitNickname(event) {
     .then((Response) => Response.json())
     .then((data) => {
       console.log(data);
-      information.innerText = `level. ${data.level}`;
+      level.innerText = `level. ${data.level}`;
+      information.appendChild(level);
       const playerID = data.accessId;
       const URL_HISTORY = `https://api.nexon.co.kr/fifaonline4/v1.0/users/${playerID}/maxdivision`;
       const URL_MATCH = `https://api.nexon.co.kr/fifaonline4/v1.0/users/${playerID}/matches?matchtype=50&offset=1&limit=5`;
+
       fetch(URL_HISTORY, {
         headers: {
           Authorization: API_KEY,
@@ -115,13 +191,15 @@ function onSubmitNickname(event) {
             if (d.divisionId === HistoryData[0].division) {
               playerHistory.innerText = d.divisionName;
               playerImage.src = `image/ico_rank${d.divisionId}.png`;
-              document.body.appendChild(playerHistory);
-              document.body.appendChild(playerImage);
+              information.appendChild(playerHistory);
+              information.appendChild(playerImage);
             }
           });
         });
-      const matchResult = [{ date: "", result: "" }];
-      //
+
+      const matchResult = [];
+      const fetchMatchPromises = [];
+
       fetch(URL_MATCH, {
         headers: {
           Authorization: API_KEY,
@@ -129,21 +207,64 @@ function onSubmitNickname(event) {
       })
         .then((Response) => Response.json())
         .then((matchData) => {
+          console.log(matchData);
           matchData.forEach((d) => {
-            const URL_MATCH_DETAIL = `https://api.nexon.co.kr/fifaonline4/v1.0/matches/${d}`;
-            fetch(URL_MATCH_DETAIL, {
-              headers: {
-                Authorization: API_KEY,
-              },
-            })
+            const fetchMatchPromise = fetch(
+              `https://api.nexon.co.kr/fifaonline4/v1.0/matches/${d}`,
+              {
+                headers: {
+                  Authorization: API_KEY,
+                },
+              }
+            )
               .then((Response) => Response.json())
               .then((detailData) => {
-                matchResult.push = {
-                  date: detailData.matchDate,
+                console.log(2);
+                let time = detailData.matchDate;
+                time = time.replace(/-/g, "");
+                time = time.replace(/T/g, "");
+                time = time.replace(/:/g, "");
+                matchResult.push({
+                  date: time,
                   result: detailData.matchInfo[0].matchDetail.matchResult,
-                };
+                });
               });
+
+            fetchMatchPromises.push(fetchMatchPromise);
+          });
+
+          Promise.all(fetchMatchPromises).then(() => {
+            matchResult.sort((a, b) => {
+              const dateA = new Date(
+                parseInt(a.date.slice(0, 4)),
+                parseInt(a.date.slice(4, 6)) - 1,
+                parseInt(a.date.slice(6, 8)),
+                parseInt(a.date.slice(8, 10)),
+                parseInt(a.date.slice(10, 12)),
+                parseInt(a.date.slice(12, 14))
+              );
+              const dateB = new Date(
+                parseInt(b.date.slice(0, 4)),
+                parseInt(b.date.slice(4, 6)) - 1,
+                parseInt(b.date.slice(6, 8)),
+                parseInt(b.date.slice(8, 10)),
+                parseInt(b.date.slice(10, 12)),
+                parseInt(b.date.slice(12, 14))
+              );
+              return dateB - dateA; // 역순으로 정렬하기 위해 dateB - dateA로 변경
+            });
+            // 여기에서 matchResult 배열이 완성된 상태이므로 원하는 작업을 수행할 수 있습니다.
             console.log(matchResult);
+            matchRecord0.innerText = `${matchResult[0].date} ${matchResult[0].result}`;
+            matchRecord1.innerText = `${matchResult[1].date} ${matchResult[1].result}`;
+            matchRecord2.innerText = `${matchResult[2].date} ${matchResult[2].result}`;
+            matchRecord3.innerText = `${matchResult[3].date} ${matchResult[3].result}`;
+            matchRecord4.innerText = `${matchResult[4].date} ${matchResult[4].result}`;
+            information.appendChild(matchRecord0);
+            information.appendChild(matchRecord1);
+            information.appendChild(matchRecord2);
+            information.appendChild(matchRecord3);
+            information.appendChild(matchRecord4);
           });
         });
     });
