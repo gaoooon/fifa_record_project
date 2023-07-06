9bc694d6952e6bc1e96b46a6f528e55db3e53816
gaoooon <gaon9749@gmail.com>
2023-07-06 21:07:38 +0900

    거의 완료

--- script.js
+++ script.js
@@ -82,10 +82,15 @@ const Division = [
 
 const inputNickname = document.getElementById("inputNickname");
 const inputSubmit = document.getElementById("inputSubmit");
-//const playerName = document.getElementById("playerName");
+const playerName = document.createElement("div");
 const playerHistory = document.createElement("div");
 const playerImage = document.createElement("img");
 const level = document.createElement("div");
+const matches = document.getElementById("matches");
+const userInformation = document.getElementById("user");
+const userName = document.getElementById("userName");
+const userRank = document.getElementById("userRank");
+
 const matchRecord0 = document.createElement("div");
 const matchRecord1 = document.createElement("div");
 const matchRecord2 = document.createElement("div");
@@ -163,8 +168,17 @@ const matchRecord4 = document.createElement("div");
 function onSubmitNickname(event) {
   event.preventDefault();
   const Nickname = inputNickname.value;
+  if (Nickname === null) {
+    matchRecord0.remove();
+    matchRecord1.remove();
+    matchRecord2.remove();
+    matchRecord3.remove();
+    matchRecord4.remove();
+  }
   const information = document.getElementById("NicknameHistory");
-
+  console.dir(information);
+  information.style.width = "500px";
+  information.style.padding = "50px";
   const URL_NICKNAME = `https://api.nexon.co.kr/fifaonline4/v1.0/users?nickname=${Nickname}`;
   fetch(URL_NICKNAME, {
     headers: {
@@ -174,8 +188,13 @@ function onSubmitNickname(event) {
     .then((Response) => Response.json())
     .then((data) => {
       console.log(data);
+      console.log(userName);
+      playerName.innerText = `${Nickname}님의 정보`;
       level.innerText = `level. ${data.level}`;
-      information.appendChild(level);
+      userName.style.padding = "5px";
+      userName.style.marginBottom = "30px";
+      userName.appendChild(playerName);
+      userRank.appendChild(level);
       const playerID = data.accessId;
       const URL_HISTORY = `https://api.nexon.co.kr/fifaonline4/v1.0/users/${playerID}/maxdivision`;
       const URL_MATCH = `https://api.nexon.co.kr/fifaonline4/v1.0/users/${playerID}/matches?matchtype=50&offset=1&limit=5`;
@@ -191,8 +210,8 @@ function onSubmitNickname(event) {
             if (d.divisionId === HistoryData[0].division) {
               playerHistory.innerText = d.divisionName;
               playerImage.src = `image/ico_rank${d.divisionId}.png`;
-              information.appendChild(playerHistory);
-              information.appendChild(playerImage);
+              userRank.appendChild(playerHistory);
+              userRank.appendChild(playerImage);
             }
           });
         });
@@ -219,7 +238,6 @@ function onSubmitNickname(event) {
             )
               .then((Response) => Response.json())
               .then((detailData) => {
-                console.log(2);
                 let time = detailData.matchDate;
                 time = time.replace(/-/g, "");
                 time = time.replace(/T/g, "");
@@ -255,16 +273,30 @@ function onSubmitNickname(event) {
             });
             // 여기에서 matchResult 배열이 완성된 상태이므로 원하는 작업을 수행할 수 있습니다.
             console.log(matchResult);
-            matchRecord0.innerText = `${matchResult[0].date} ${matchResult[0].result}`;
-            matchRecord1.innerText = `${matchResult[1].date} ${matchResult[1].result}`;
-            matchRecord2.innerText = `${matchResult[2].date} ${matchResult[2].result}`;
-            matchRecord3.innerText = `${matchResult[3].date} ${matchResult[3].result}`;
-            matchRecord4.innerText = `${matchResult[4].date} ${matchResult[4].result}`;
-            information.appendChild(matchRecord0);
-            information.appendChild(matchRecord1);
-            information.appendChild(matchRecord2);
-            information.appendChild(matchRecord3);
-            information.appendChild(matchRecord4);
+            // for (let i = 0; i <= 4; i++) {
+            //   const matchRecord = document.createElement("div");
+            //   matchRecord.className = "match";
+            //   matchRecord.innerText = `${number} ${matchResult[i].result}`;
+            //   matches.appendChild(matchRecord);
+            // }
+            const record = document.createElement("div");
+            record.innerText = "최근 전적";
+            matches.appendChild(record);
+            const number0 = matchResult[0].date.substr(0, 8);
+            const number1 = matchResult[1].date.substr(0, 8);
+            const number2 = matchResult[2].date.substr(0, 8);
+            const number3 = matchResult[3].date.substr(0, 8);
+            const number4 = matchResult[4].date.substr(0, 8);
+            matchRecord0.innerText = `${number0} ${matchResult[0].result}`;
+            matchRecord1.innerText = `${number1} ${matchResult[1].result}`;
+            matchRecord2.innerText = `${number2} ${matchResult[2].result}`;
+            matchRecord3.innerText = `${number3} ${matchResult[3].result}`;
+            matchRecord4.innerText = `${number4} ${matchResult[4].result}`;
+            matches.appendChild(matchRecord0);
+            matches.appendChild(matchRecord1);
+            matches.appendChild(matchRecord2);
+            matches.appendChild(matchRecord3);
+            matches.appendChild(matchRecord4);
           });
         });
     });
